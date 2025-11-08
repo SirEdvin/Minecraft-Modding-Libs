954587946a9fd774f0a3a9d51c5863d9dad7b402
SirEdvin <me@siredvin.site>
2025-11-08 21:18:41 +0200

    refactor: change package for block

--- projects/tweakium-core/src/main/kotlin/site/siredvin/tweakium/modules/minecraft/block/StatefulPeripheralNBTBlock.kt
+++ projects/tweakium-core/src/main/kotlin/site/siredvin/tweakium/modules/minecraft/block/StatefulPeripheralNBTBlock.kt
@@ -1,4 +1,4 @@
-package site.siredvin.tweakium.modules.minecraft.blockentity
+package site.siredvin.tweakium.modules.minecraft.block
 
 import site.siredvin.broccolium.modules.base.api.ISavableBlockEntity
 import site.siredvin.broccolium.modules.base.block.BaseNBTBlock
@@ -12,5 +12,5 @@ abstract class StatefulPeripheralNBTBlock<T, V : IOwnedPeripheral<*>>(
     properties: Properties = BlockUtil.defaultProperties(),
 ) : BaseNBTBlock<T>(belongToTickingEntity, properties) where T : PeripheralBlockEntity<V>, T : ISavableBlockEntity {
     override val internalDataTag: String
-        get() = StatefulPocketUpgrade.STORED_DATA_TAG
+        get() = StatefulPocketUpgrade.Companion.STORED_DATA_TAG
 }
