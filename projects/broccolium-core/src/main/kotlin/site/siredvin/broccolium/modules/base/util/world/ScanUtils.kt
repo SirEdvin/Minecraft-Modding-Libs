ce23833e34f3fe924ed37b674eafeb5814e56e26
GravityIO <Harryioannoucy@gmail.com>
2026-03-07 19:59:56 +0200

    fix entity scanning radius

--- projects/broccolium-core/src/main/kotlin/site/siredvin/broccolium/modules/base/util/world/ScanUtils.kt
+++ projects/broccolium-core/src/main/kotlin/site/siredvin/broccolium/modules/base/util/world/ScanUtils.kt
@@ -3,10 +3,25 @@ package site.siredvin.broccolium.modules.base.util.world
 import net.minecraft.core.BlockPos
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.phys.AABB
 import java.util.function.BiConsumer
 import java.util.function.Predicate
 
 object ScanUtils {
+
+    fun getBox(pos: BlockPos, radius: Double): AABB {
+        val center = pos.center
+        val r = radius + 0.5
+
+        return AABB(
+            center.x - r,
+            center.y - r,
+            center.z - r,
+            center.x + r,
+            center.y + r,
+            center.z + r,
+        ).inflate(0.99)
+    }
 
     fun traverseBlocks(
         world: Level,
